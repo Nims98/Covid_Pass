c7f564a122cc555210bb7f4401f82612dff094da
Nims98 <nirmalasabaragamuwa@gmail.com>
2022-08-20 16:45:18 +0530

    auth guards modified

--- client/src/app/auth/guards/auth.guard.ts
+++ client/src/app/auth/guards/auth.guard.ts
@@ -11,7 +11,6 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate() {
-    console.log("asdasdasd")
     if (this.authService.isLoggedIn()) {
       return true;
     }
